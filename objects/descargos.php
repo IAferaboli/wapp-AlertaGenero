f4a7d7c54ddd29435163a993e12c1fef0b0a28ca
IAferaboli <iagferaboli@gmail.com>
2023-07-10 19:41:52 -0300

    update descargos.php

--- objects/descargos.php
+++ objects/descargos.php
@@ -6,14 +6,26 @@ require_once "usuarie.php";
 
 class Descargo extends Conexion {
 
-    public $id_descargo, $nombre, $apellido, $id_altura, $id_cutis, $id_colorpelo, $relacion, $id_tatoo, $id_cicatriz, $id_discap;
+    public $id_descargo, $nombre, $apellido, $dni, $id_altura, $id_cutis, $id_colorpelo, $relacion, $id_tatoo, $id_cicatriz, $id_discap, $id_tipo, $id_modalidad, $descrip;
 
-    //Create - Cargar
-    public function create()
-    {
-        $this->conectar();
-        $prepare = mysqli_prepare($this->conect, "INSERT INTO descargos (nombre, apellido, id_altura, id_cutis, id_colorpelo, relacion, id_tatoo, id_cicatriz, id_discap) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        $prepare->bind_param("ssiiisiii", $this->nombre, $this->apellido, $this->id_altura, $this->id_cutis, $this->id_colorpelo, $this->relacion, $this->id_tatoo, $this->id_cicatriz, $this->id_discap);
+
+    public static function getUsuarie($id_usuarie){
+        $conexion = new Conexion();
+        $conexion->conectar();
+        $prepare = mysqli_prepare($conexion->conect, "SELECT * FROM usuaries WHERE id_usuarie = ?");
+        $prepare->bind_param("i", $id_usuarie);
         $prepare->execute();
+        $resultado = $prepare->get_result();
+        return $resultado->fetch_object(Usuarie::class);
+    }
+    
+    public static function getAgresor($id_agresor){
+        $conexion = new Conexion();
+        $conexion->conectar();
+        $prepare = mysqli_prepare($conexion->conect, "SELECT * FROM agresores WHERE id_agresores = ?");
+        $prepare->bind_param("i", $id_agresor);
+        $prepare->execute();
+        $resultado = $prepare->get_result();
+        return $resultado->fetch_object(Agresor::class);
     }
 }
