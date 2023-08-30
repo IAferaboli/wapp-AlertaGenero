0b97c63f18bb9cb5bb076430d95309a47fe95487
IAferaboli <iagferaboli@gmail.com>
2023-08-29 21:43:20 -0300

    Avances en entidad agresor desde models, controlador y view

--- models/agresores.php
+++ models/agresores.php
@@ -1,28 +1,45 @@
 <?php
-
+//revisar todos los atributos en relación a la DB
 require_once "conexion.php";
 
 class Agresor extends Conexion {
 
-    public $id_agresor, $nombre, $apellido, $id_altura, $id_cutis, $id_colorpelo, $relacion, $id_tatoo, $id_cicatriz, $id_discap, $alfanum, $reconoce;
+    public $id_agresor, $nombre, $apellido, $id_altura, $id_pelo, $id_tatoo, $id_cicatriz, $id_discap, $alfanum, $alturaSeleccionada;
 
     
-    public function codearagresor($nombre, $apellido, $id_agresor){
-        $name = substr($nombre, 0, 3);
-        $apell = substr($apellido, -3, 3);
-        $id = substr($id_agresor, 0, 1);
+    // public function codearagresor($nombre, $apellido, $id_agresor){
+    //     $name = substr($nombre, 0, 3);
+    //     $apell = substr($apellido, -3, 3);
+    //     $id = substr($id_agresor, 0, 1);
 
-        $alfanum = $name . $apell . $id;
+    //     $alfanum = $name . $apell . $id;
 
-        return $alfanum;
-    }
+    //     return $alfanum;
+    // }
     
     //Create - Cargar
     public function create()
     {
         $this->conectar();
-        $prepare = mysqli_prepare($this->conect, "INSERT INTO agresores (nombre, apellido, id_altura, id_cutis, id_colorpelo, relacion, id_tatoo, id_cicatriz, id_discap) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        $prepare->bind_param("ssiiisiii", $this->nombre, $this->apellido, $this->id_altura, $this->id_cutis, $this->id_colorpelo, $this->relacion, $this->id_tatoo, $this->id_cicatriz, $this->id_discap);
+        $prepare = mysqli_prepare($this->conect, "INSERT INTO agresores (nombre, apellido, id_altura, id_pelo, id_tatoo, id_cicatriz, id_discap) VALUES ( ?, ?, ?, ?, ?, ?, ?)");
+        $prepare->bind_param("ssiiiii", $this->nombre, $this->apellido, $this->id_altura, $this->id_pelo, $this->id_tatoo, $this->id_cicatriz, $this->id_discap);
         $prepare->execute();
+    }
+
+    public function getAltura(){
+        $conexion = new Conexion();
+        $conexion->conectar();
+        $prepare = mysqli_prepare($conexion->conect, "SELECT * FROM altura WHERE id_altura IN (SELECT id_altura FROM agresores WHERE id_altura =?)");
+        $prepare->bind_param("i", $this->id_altura);
+        $prepare->execute();
+        $resposta = $prepare->get_result();
+       
+        $alturas = array();
+
+        while($altura = $resposta->fetch_object(Altura::class)) {
+            array_push($alturas, $altura);
+        }
+
+        return $alturas;
     }
 }
--- models/altura.php
+++ models/altura.php
@@ -6,4 +6,29 @@ class Altura extends Conexion {
 
     public $altura, $id_altura;
 
+    public static function getAltura($id_altura){
+        $conexion = new Conexion();
+        $conexion->conectar();
+        $prepare = mysqli_prepare($conexion->conect, "SELECT * FROM altura WHERE id_altura = ?");
+        $prepare->bind_param("i", $id_altura);
+        $prepare->execute();
+        $resultado = $prepare->get_result();
+        return $resultado->fetch_object(Altura::class);
+    }
+
+    public static function getAlturas(){
+        $conexion = new Conexion();
+        $conexion->conectar();
+        $prepare = mysqli_prepare($conexion->conect, "SELECT altura, id_altura FROM altura");
+        $prepare->execute();
+        $resposta = $prepare->get_result();
+
+        $alturas = array();
+        while($altura = $resposta->fetch_object(Altura::class)) {
+            array_push($alturas, $altura);
+        }
+
+        return $alturas;
+    }
+
 }
