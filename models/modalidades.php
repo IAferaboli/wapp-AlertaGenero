79c1d1b571a097aa9ec463e0261f6c47c804f9a7
IAferaboli <iagferaboli@gmail.com>
2023-09-26 21:31:37 -0300

    modalidades

--- models/modalidades.php
+++ models/modalidades.php
@@ -6,4 +6,30 @@ class Modalidades extends Conexion {
 
     public $nombre, $id_modalidad;
 
+    public static function getModalidad($id_modalidad){
+        $conexion = new Conexion();
+        $conexion->conectar();
+        $prepare = mysqli_prepare($conexion->conect, "SELECT * FROM modalidades WHERE id_modalidad = ?");
+        $prepare->bind_param("i", $id_modalidad);
+        $prepare->execute();
+        $resultado = $prepare->get_result();
+        return $resultado->fetch_object(Modalidades::class);
+    }
+
+    public static function getModalidades(){
+        $conexion = new Conexion();
+        $conexion->conectar();
+        $prepare = mysqli_prepare($conexion->conect, "SELECT nombre, id_modalidad FROM modalidades");
+        $prepare->execute();
+        $resposta = $prepare->get_result();
+
+        $modalidades = array();
+        while($modalidad = $resposta->fetch_object(Modalidades::class)) {
+            array_push($modalidades, $modalidad);
+        }
+
+        return $modalidades;
+    }
+
+
 }
