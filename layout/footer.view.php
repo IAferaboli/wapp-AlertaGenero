04e82ca8f6858fc9975a8e1ed812205018c01a1d
EstebanL650 <estebanledesma650@gmail.com>
2023-10-10 16:49:37 -0300

    Probando Links-redireccionamiento
    Agregado de instagram, soporte, etc.

--- layout/footer.view.php
+++ layout/footer.view.php
@@ -1,14 +1,14 @@
 <footer>
     <ul class="social_icon"><!--clase hecha para la vinvulacion de iconos externos-->
-        <li><a href="" target="_blank"><ion-icon name="logo-facebook"></ion-icon></a></li>
-        <li><a href="" target="_blank"><ion-icon name="logo-whatsapp"></ion-icon></a></li>
-        <li><a href="" target="_blank"><ion-icon name="logo-instagram"></ion-icon></a></li>
+        <li><a href="https://www.instagram.com/alertagenero/" target="_blank"><ion-icon name="logo-facebook"></ion-icon></a></li>
+        <li><a href="api.whatsapp.com/send/?phone=543400510046&text=Hola%21%21%2C+quisiera+saber+mas+sobre+Alerta+Genero&type=phone_number&app_absent=0" target="_blank"><ion-icon name="logo-whatsapp"></ion-icon></a></li>
+        <li><a href="https://www.instagram.com/alertagenero/" target="_blank"><ion-icon name="logo-instagram"></ion-icon></a></li>
     </ul>
     <ul class="menu"><!--Menu secundario-->
         <li><a href="../controladores/createUsuarie.php">Inicio</a></li>
         <!-- <li><a href="" target="_blank">Ubicacion</a></li> -->
-        <li><a href="">Soporte</a></li>
-        <li><a href="#">Contacto</a></li>
+        <li><a href="mailto:Alerta.Genero.Wapp01@gmail.com?subject=Tengo%20un%20problema%20con%20la%20pagina">Soporte</a></li>
+        <li><a href="https://www.instagram.com/alertagenero/">Contacto</a></li>
     </ul>
     <p>©2023 Alerta Genero <br> Todos los derechos resevados.</p>
 </footer>
